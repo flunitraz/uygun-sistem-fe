fb3f8b03b83e1d0c61bb67f1341d9148890b4bcd
Yusuf Felekoglu <yusuf.felekoglu@outlook.com>
2024-07-07 04:29:56 +0300

    mongodb integration

--- app/api/getProds/route.ts
+++ app/api/getProds/route.ts
@@ -1,31 +1,56 @@
-import axios from 'axios';
-var cron = require('node-cron');
+import axios from "axios";
+var cron = require("node-cron");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-let lastFetchedData = [{
-  islemci: 'fetching',
-  ram: 'fetching',
-  ekran_karti:'fetching',
-  depolama:'fetching',
-}];
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const fetchDataFromRemote = async () => {
+const dataSchema = new mongoose.Schema({
+  islemci: String,
+  ram: String,
+  ekran_karti: String,
+  depolama: String,
+  fiyat: String,
+  satici: String,
+  img: String,
+  url: String,
+});
+
+const Data = mongoose.models.prods || mongoose.model("prods", dataSchema);
+
+const updateDb = async () => {
   try {
-    const response = await axios.get('https://uygun-sistem-be.onrender.com/scrape/incehesap');
-    lastFetchedData = response.data;
-    console.log('Data fetched successfully');
+    const response = await axios.get(
+      "https://uygun-sistem-be.onrender.com/scrape/incehesap"
+    );
+    const fetchedData = response.data;
+
+    await Data.deleteMany({});
+    await Data.insertMany(fetchedData);
+    console.log("Data fetched successfully and saved to MongoDB");
   } catch (error) {
-    console.error('Error fetching data');
+    console.error("Error fetching data or saving to MongoDB");
     throw error;
   }
 };
-cron.schedule('0 * * * *', async () => {
+
+cron.schedule("0 * * * *", async () => {
   try {
-    await fetchDataFromRemote();
+    await updateDb();
   } catch (error) {
-    console.error('Failed to fetch data in cron job');
+    console.error("Failed to fetch data in cron job");
   }
 });
 
 export async function GET() {
-  return Response.json(lastFetchedData);
+  try {
+    const fetchedData = await Data.find({}).exec();
+    return Response.json(fetchedData);
+  } catch (error) {
+    console.error("Failed to fetch data from MongoDB");
+    throw error;
+  }
 }
