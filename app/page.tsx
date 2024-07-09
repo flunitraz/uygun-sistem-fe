4e4b995dee09732b4f615a8f5e13ff7c79a496a4
Yusuf Felekoglu <yusuf.felekoglu@outlook.com>
2024-07-09 21:33:59 +0300

    fresh deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,18 @@ import ListView from "./components/listView";
 import Filter from "./components/filter";
 import TableTopBar from "./components/tableTopBar";
 import axios from "axios";
-// async function getServerSideProps() {
-//   const res = await axios.get("/api/getProds");
-//   const repo: Prod[] = await res.data();
-//   return repo;
-// }
-// const getServerSideProps = async () => {
-//   try {
-//     const response = await axios.get(process.env.URL+"/api/getProds");
-//     return(response.data);
-//   } catch (error) {
-//     console.error("Error fetching data");
-//     throw error;
-//   }
-// };
+
+const getServerSideProps = async () => {
+  try {
+    const response = await axios.get(process.env.URL+"/api/getProds");
+    return(response.data);
+  } catch (error) {
+    console.error("Error fetching data");
+    throw error;
+  }
+};
 export default async function Page() {
-// const prods = await getServerSideProps();
-const prods = [{} as Prod]
+const prods = await getServerSideProps();
 
   return (
     <>
